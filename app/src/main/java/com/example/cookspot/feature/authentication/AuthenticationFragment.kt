faeda035079460128342d57fbde74f4cbf96d5ae
Ianis_Teja <ianist2001@gmail.com>
2023-03-27 22:23:45 +0300

    Added Firebase dependencies to the project

--- app/src/main/java/com/example/cookspot/feature/authentication/AuthenticationFragment.kt
+++ app/src/main/java/com/example/cookspot/feature/authentication/AuthenticationFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.cookspot.R
+import com.example.cookspot.databinding.FragmentAuthenticationBinding
+import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 
-class AuthenticationFragment:Fragment(R.layout.fragment_authentication) {
+class AuthenticationFragment : Fragment(R.layout.fragment_authentication) {
+
+    private val binding by viewBinding(FragmentAuthenticationBinding::bind)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
